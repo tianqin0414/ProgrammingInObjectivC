585702a7cfb0ca3959a52546c4d1eb63595fba34
ZeroIos <38795697@qq.com>
2018-04-14 15:55:24 +0800

    Initial Commit

---
+++ ProgrammingInObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProgrammingInObjectiveC
+//
+//  Created by Tianq on 2018/4/14.
+//  Copyright © 2018年 Tianq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
